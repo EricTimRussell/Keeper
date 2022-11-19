1cddfd2b38199eb571b3a010abf987f0151b8cda
EricTimRussell <erictim.russell@live.com>
2022-11-19 15:08:19 -0700

    render setup

--- Keeper.client/vite.config.js
+++ Keeper.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../keeper/client',
     sourcemap: false
   },
   server: {
