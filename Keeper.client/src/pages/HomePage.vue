39acfd0f36fb95e6fcc3359029268b3beeb89730
Eric Russell <Erictim.Russell@live.com>
2022-11-22 12:21:38 -0700

    Attempt at masonry

--- Keeper.client/src/pages/HomePage.vue
+++ Keeper.client/src/pages/HomePage.vue
@@ -1,10 +1,9 @@
 <template>
 
   <body class="container-fluid">
-    <div class="row margin my-5">
-      <div class="col-md-3 col-sm-6 my-3" v-for="k in keeps" :key="k.id">
-        <!-- Inject KeepCard -->
-        <KeepCard :keeps="k" />
+    <div class="row justify-content-center">
+      <div class="col-md-10 col-12 masonry">
+        <KeepCard :keeps="k" v-for="k in keeps" :key="k.id" />
       </div>
     </div>
   </body>
@@ -44,9 +43,34 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.margin {
-  margin-left: 15rem;
-  margin-right: 15rem;
+body {
+  margin: 0;
+  padding: 1rem;
+}
+
+.masonry {
+  columns: 4;
+  column-gap: 2rem;
+
+  img.photo {
+    width: 20vw;
+    margin-top: 2rem
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .masonry {
+    columns: 2;
+    margin-left: 2rem;
+    margin-right: 2rem;
+  }
+
+  .margin {
+    margin-left: 15rem;
+    margin-right: 15rem;
+  }
+
+
 }
 </style>
 
